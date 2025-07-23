e33b8e90bec53b3032e13da857fcedc7a1bf6bb6
jkhiba2023 <khan.jabir1055@gmail.com>
2025-07-23 18:59:51 +0530

    Arithmetic Operation

---
+++ Day3/script.js
@@ -0,0 +1,9 @@
+/*Get user to input two number using prompt and print their possible arithmetic result*/
+
+let a = Number(prompt("Enter Number From 1 to 300"));
+let b = Number(prompt("Enter Number from 1 to 300"));
+console.log("a+b=", a + b);
+console.log("a-b=", a - b);
+console.log("a/b=", a / b);
+console.log("a*b=", a * b);
+console.log("a%b=", a % b);
