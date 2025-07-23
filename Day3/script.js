732cd780bdb2bb69fc19f805dce6b2f2d7be5075
jkhiba2023 <khan.jabir1055@gmail.com>
2025-07-23 20:31:00 +0530

    Check the range

--- Day3/script.js
+++ Day3/script.js
@@ -13,3 +13,25 @@
 // let num = Number(prompt("Enter Number to check even or odd"));
 // let result = (num % 2 == 0) ? ("Even number") : ("Odd number");
 // console.log(result);
+
+/*let x=5;
+     x+=3;
+     x-=2;
+     x*=4;
+     x/=6;
+     x%=3 = 1 */
+
+
+// let x = 5;
+// x += 3;
+// x -= 2;
+// x *= 4;
+// x /= 6;
+// x %= 3;
+// console.log(x);
+
+/*check if a number is within a range between 10 and 20*/
+
+let range = Number(prompt("Enter number from 10 to 20"));
+let result = (range > 10 && range < 20) ? ("number lies between 10 to 20") : ("Number is either less than 10 or more than 20");
+console.log(result);
