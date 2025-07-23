a4ef6b083007f329fd1a979fe6f303b439d5b38d
jkhiba2023 <khan.jabir1055@gmail.com>
2025-07-23 19:12:48 +0530

    Even odd using ternary operators

--- Day3/script.js
+++ Day3/script.js
@@ -1,9 +1,15 @@
 /*Get user to input two number using prompt and print their possible arithmetic result*/
 
-let a = Number(prompt("Enter Number From 1 to 300"));
-let b = Number(prompt("Enter Number from 1 to 300"));
-console.log("a+b=", a + b);
-console.log("a-b=", a - b);
-console.log("a/b=", a / b);
-console.log("a*b=", a * b);
-console.log("a%b=", a % b);
+// let a = Number(prompt("Enter Number From 1 to 300"));
+// let b = Number(prompt("Enter Number from 1 to 300"));
+// console.log("a+b=", a + b);
+// console.log("a-b=", a - b);
+// console.log("a/b=", a / b);
+// console.log("a*b=", a * b);
+// console.log("a%b=", a % b);
+
+/*Get user to input a number using prompt and check whether even or odd using ternary operator*/
+
+// let num = Number(prompt("Enter Number to check even or odd"));
+// let result = (num % 2 == 0) ? ("Even number") : ("Odd number");
+// console.log(result);
