d70f5131a0c4447fbf18a79c1881042964da9a31
jkhiba2023 <khan.jabir1055@gmail.com>
2025-07-24 18:12:58 +0530

    Second code for raining

--- Day4/script.js
+++ Day4/script.js
@@ -1,8 +1,17 @@
 /*WAP to check if the stock of a product is 0. If it is, display the message "Product is out of stock".*/
 
-let productInStock = 0;
-if (productInStock === 0) {
-  console.log("Product is out of stock.")
+// let productInStock = 0;
+// if (productInStock === 0) {
+//   console.log("Product is out of stock.")
+// } else {
+//   console.log(`Number of Product in stock is ${productInStock}`);
+// }
+
+/*If its raining, take an umbrella. Otherwise enjoy the sunshine.*/
+
+let wheather = "Rainy";
+if (wheather == "Rainy") {
+  console.log("Take Umbrella");
 } else {
-  console.log(`Number of Product in stock is ${productInStock}`);
+  console.log("Enjoy the sunshine");
 }
