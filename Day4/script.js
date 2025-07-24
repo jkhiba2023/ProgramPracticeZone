7b0417da6fb18eefd3cd14c58a500a756e58b8ef
jkhiba2023 <khan.jabir1055@gmail.com>
2025-07-24 17:29:33 +0530

    First Program Day 4 Regular

---
+++ Day4/script.js
@@ -0,0 +1,8 @@
+/*WAP to check if the stock of a product is 0. If it is, display the message "Product is out of stock".*/
+
+let productInStock = 0;
+if (productInStock === 0) {
+  console.log("Product is out of stock.")
+} else {
+  console.log(`Number of Product in stock is ${productInStock}`);
+}
