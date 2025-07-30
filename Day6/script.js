437402e00a5e8a5e89519b2d2e222afb8ef399df
jkhiba2023 <khan.jabir1055@gmail.com>
2025-07-30 20:21:13 +0530

    Simple calculator

---
+++ Day6/script.js
@@ -0,0 +1,35 @@
+/*Create a simple calculator. Take two numbers and an operator (+,-,*,/)
+and calculate the result using switch.*/
+
+// let n1 = 25;
+// let n2 = 5;
+// let operators = prompt("Enter the operation");
+
+// switch (operators) {
+//   case "+":
+//     let add = n1 + n2;
+//     console.log(add);
+//     break;
+//   case "-":
+//     let sub = n1 - n2;
+//     console.log(sub);
+//     break;
+//   case "*":
+//     let multi = n1 * n2;
+//     console.log(multi);
+//     break;
+//   case "/":
+//     let div = n1 / n2;
+//     console.log(div);
+//     break;
+//   case "%":
+//     let rem = n1 % n2;
+//     console.log(rem);
+//     break;
+//   default:
+//     console.log("Invalid Input");
+//     break;
+// }
+
+
+
