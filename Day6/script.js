326af5553935b596036bd962bb6f8e04736da8b9
jkhiba2023 <khan.jabir1055@gmail.com>
2025-07-30 23:20:16 +0530

    ATM program

--- Day6/script.js
+++ Day6/script.js
@@ -32,4 +32,44 @@ and calculate the result using switch.*/
 // }
 
 
+/*Create a Simple ATP Program
+ user can choose:
+   1. Check Balance
+   2. Deposit
+   3. Withdraw
+   4. Exit.
+
+   Note that in case of "Deposit" if the deposit amount is less than 1Rs produce error otherwise deposit the amount
+   and show the message with a new balance. And in case of "Withdraw" if withdraw amount is greater than balance then
+   or less than 1rs than produce error otherwise withdraw amount and show remaining balance.
+*/
+
+// let currentBal = 5000;
+// let seletChoice = Number(prompt("Please Select Your Choice 1.Check Balanc 2.Deposite 3.Withdraw 4.Exit"));
+
+// if (seletChoice === 1) {
+//   console.log(`Your current balance is ${currentBal}`);
+// } else if (seletChoice === 2) {
+//   let depositAmount = Number(prompt("Enter Your Deposite Amount"));
+//   if (depositAmount > 0) {
+//     currentBal += depositAmount;
+//     console.log(`Your current balance is ${currentBal}`);
+//   } else {
+//     console.log("Invalid Amount")
+//   }
+// } else if (seletChoice === 3) {
+//   let withdrawAmount = Number(prompt("Enter your withdraw amount"));
+//   if (withdrawAmount > 0 && withdrawAmount <= currentBal) {
+//     currentBal -= withdrawAmount;
+//     console.log(`Your current balance is ${currentBal}`);
+//   } else if (withdrawAmount > currentBal) {
+//     console.log(`You Have Insufficient Balance`);
+//   } else {
+//     console.log("Invalide Valid Withdrawel amount");
+//   }
+// }
+// else {
+//   console.log("Thank You For Taking Our Service");
+// }
+
 
