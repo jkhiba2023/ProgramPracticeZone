8e0a7b6cf366972d9c838cce4308eac1d89260b3
jkhiba2023 <khan.jabir1055@gmail.com>
2025-08-01 00:48:05 +0530

    Sum from m to n

---
+++ Day8/script.js
@@ -0,0 +1,8 @@
+/*Calculate the sum of numbers from 'm' to 'n'*/
+// let n = 10;
+// let sum = 0;
+// for (let m = 1; m <= n; m++) {
+//   console.log(m, "+", sum);
+//   sum += m;
+// }
+// console.log(sum);
