84ff220af1658bdcbb78d78a022747b5a6f62303
jkhiba2023 <khan.jabir1055@gmail.com>
2025-08-01 01:05:50 +0530

    Factorial without using function

--- Day8/script.js
+++ Day8/script.js
@@ -15,3 +15,11 @@
 //     console.log(`This is an odd number ${i}`);
 //   }
 // }
+
+/*Create to program to find the factorial of n*/
+
+// let mul = 1;
+// for (let i = 1; i <= 10; i++) {
+//   mul *= i;
+// }
+// console.log(mul);
