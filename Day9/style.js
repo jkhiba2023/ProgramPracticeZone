b0d63ad54c465e5bdf0a540013584663f11e8afb
jkhiba2023 <khan.jabir1055@gmail.com>
2025-08-04 02:04:43 +0530

    Password Checker

--- Day9/style.js
+++ Day9/style.js
@@ -1,25 +1,47 @@
 /*Create a "Number Knock" game. (Ask the user to keep guessing the number until the user enters correct guess).*/
 
-let computerNumber = Math.floor(Math.random() * 10 + 1);
-let guessNumber = null;
+// let computerNumber = Math.floor(Math.random() * 10 + 1);
+// let guessNumber = null;
 
-while (guessNumber !== computerNumber) {
-  guessNumber = Number(prompt("Enter a number from 0 to 10"));
+// while (guessNumber !== computerNumber) {
+//   guessNumber = Number(prompt("Enter a number from 0 to 10"));
 
-  if (isNaN(guessNumber)) {
-    alert("Invalid input, please try again!");
-    continue; // Skip rest of the loop and ask again
-  }
+//   if (isNaN(guessNumber)) {
+//     alert("Invalid input, please try again!");
+//     continue; // Skip rest of the loop and ask again
+//   }
 
-  if (guessNumber < computerNumber) {
-    alert("Guessing number is too low, please try again!");
-  } else if (guessNumber > computerNumber) {
-    alert("Guessing number is too high, please try again!");
-  } else {
-    alert("Congratulations! You guessed the correct number...");
-  }
-}
+//   if (guessNumber < computerNumber) {
+//     alert("Guessing number is too low, please try again!");
+//   } else if (guessNumber > computerNumber) {
+//     alert("Guessing number is too high, please try again!");
+//   } else {
+//     alert("Congratulations! You guessed the correct number...");
+//   }
+// }
+
+
 /*Simple Password Checker (Fixed Attempts).*/
+
+// let passwords = "hello123";
+// let numberOfAttempts = 0;
+// let userInput = null;
+
+// while (userInput !== passwords && numberOfAttempts < 3) {
+
+//   userInput = prompt("Enter your password: ");
+//   numberOfAttempts++;
+
+//   if (userInput === passwords) {
+//     alert("Your password is correct");
+//     break;
+//   } else if (numberOfAttempts >= 3) {
+//     alert("You have maximum attemp, Sorry !!!")
+//   } else {
+//     alert("Your password is incorrect");
+//   }
+// }
+
 
 /*Print the following Pattern. (Build it for nth numbers)
 1
