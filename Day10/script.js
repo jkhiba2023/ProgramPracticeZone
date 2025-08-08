2e5c34284c2a45689b65b990ea5a9b7a6139e95f
jkhiba2023 <khan.jabir1055@gmail.com>
2025-08-08 18:39:01 +0530

    Show an alert message that says Please login after 5 seconds on your website.

--- Day10/script.js
+++ Day10/script.js
@@ -15,16 +15,16 @@ setTimeout(function () {
 
 /*Show an alert message that says "Please login* after 5 seconds on your website.
 */
-// setTimeout(function () {
-//   alert("Please Login!!!");
-// }, 5000);
+setTimeout(function () {
+  alert("Please Login!!!");
+}, 5000);
 
 /*Make an arrow function that takes a price and a discount and returns the price after discount.*/
 
-const finalPrice = (originalPrice, discountPercentage) => {
-  let discountAmount = (originalPrice * discountPercentage) / 100;
-  let finalPrice = originalPrice - discountAmount;
-  return finalPrice;
-}
+// const finalPrice = (originalPrice, discountPercentage) => {
+//   let discountAmount = (originalPrice * discountPercentage) / 100;
+//   let finalPrice = originalPrice - discountAmount;
+//   return finalPrice;
+// }
 
-console.log(finalPrice(200, 10));
+// console.log(finalPrice(200, 10));
