d1b7b2592965065a036476ad1b1b8292fa7500bb
jkhiba2023 <khan.jabir1055@gmail.com>
2025-08-12 12:16:34 +0530

    Update

---
+++ Day11/script.js
@@ -0,0 +1,11 @@
+/*Write a function that takes two numbers (a and b) as argument. Sum a and b. Return the result.*/
+
+// function sum(a, b) {
+//   let result = a + b;
+//   return result;
+// }
+
+// console.log(sum(11, 15));
+
+
+
