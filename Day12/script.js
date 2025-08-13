3fe433a92eacf54e17bdd675a24e1a9c7382681c
jkhiba2023 <khan.jabir1055@gmail.com>
2025-08-13 16:28:14 +0530

    generate username

---
+++ Day12/script.js
@@ -0,0 +1,8 @@
+/*Create a program to take full name from user and generate a username start with @,
+followed by their full name and ends with underscore followed by the length of full name.*/
+
+// function generateUsername(str) {
+//   return `@${str}-${str.length}`;
+// }
+
+// console.log(generateUsername("JabirKhan"));
