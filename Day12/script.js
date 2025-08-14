ce6467ef5e7aef1d3a45f83e71617b8ddd3566d1
jkhiba2023 <khan.jabir1055@gmail.com>
2025-08-14 15:20:49 +0530

    count words in an strings

--- Day12/script.js
+++ Day12/script.js
@@ -6,3 +6,27 @@ followed by their full name and ends with underscore followed by the length of f
 // }
 
 // console.log(generateUsername("JabirKhan"));
+
+/*Take a string and a character from the user and :
+1. Count how many times that character appears in the string.
+2. Case-insensitive version.
+3. Find all occurrence position.
+*/
+
+// let str = prompt("Enter string.");
+// let ch = prompt("Enter character to find from string.");
+// let countLetters = 0;
+// for (let index in str) {
+//   if (str[index].toLowerCase() === ch.toLowerCase()) {
+//     console.log("position", index);
+//     countLetters++;
+//   }
+
+// }
+// console.log(countLetters);
+
+/*Count the words present in a given string.*/
+
+// let str = "Hello Raju Babu Kya haal hai??";
+// console.log(str.split(" "));
+
