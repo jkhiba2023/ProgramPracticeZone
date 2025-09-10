bd9537f95622cd732b8143bac66dbda8d80626d7
jkhiba2023 <khan.jabir1055@gmail.com>
2025-09-10 18:48:22 +0530

    Find the discounted amount

---
+++ Day1/script.js
@@ -0,0 +1,25 @@
+/*On a shopping website, calculate the total cost of a product when given the price per item (price=150) and the
+quantity (quantity=3). Also, calculate a 10% discount on the total cost and display the discounted price.*/
+
+// function discount(price, quantity) {
+//   let currentPrice = price * quantity;
+//   let discountPrice = currentPrice * 0.1;
+//   let actualPrice = currentPrice - discountPrice;
+//   console.log(actualPrice);
+// }
+
+// discount(150, 3);
+// discount(250, 4);
+// discount(1000, 5);
+
+// let price = 150;
+// let quantity = 3;
+// let totalPrice = price * quantity;
+// let discountPrice = totalPrice * 0.10;
+// let finalPrice = totalPrice - discountPrice;
+// console.log(price);
+// console.log(totalPrice);
+// console.log(discountPrice);
+// console.log(finalPrice);
+
+/*Write a program to add two numbers given by the user.*/
