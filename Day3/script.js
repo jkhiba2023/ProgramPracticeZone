abb6ee8b6587a7b4eea5d32ebbc5afdd047403ea
jkhiba2023 <khan.jabir1055@gmail.com>
2025-09-17 01:52:53 +0530

    check whether even or odd using ternary operator

--- Day3/script.js
+++ Day3/script.js
@@ -8,7 +8,12 @@
 // console.log(a / b);
 // console.log(a % b);
 
+/*Get user to input a number using prompt and check whether even or odd using ternary operator*/
 
+// let num = Number(prompt("Enter number"));
+// let result = num % 2 == 0
+//   ? "Even number" : "Odd number";
+// console.log(result);
 
 
 
