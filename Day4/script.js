dba8cbfd7efb542f8203aa3e1a655321ad2a113a
jkhiba2023 <khan.jabir1055@gmail.com>
2025-09-18 01:35:17 +0530

    Weather Update

--- Day4/script.js
+++ Day4/script.js
@@ -1,10 +1,22 @@
 /*Write a program to check if the stock of a product is 0. If it is, display the message
 "Product is out of stock."*/
 
-let ProductStock = 0;
-if (ProductStock === 0) {
-  console.log("Product is out of stock");
-} else {
-  console.log("Stock count is:", ProductStock);
+// let ProductStock = 0;
+// if (ProductStock === 0) {
+//   console.log("Product is out of stock");
+// } else {
+//   console.log("Stock count is:", ProductStock);
 
-}
+// }
+
+/*If its raining, take umbrella. Otherwise, enjoy the sunshine!!!*/
+
+// let weather = "Sunny";
+
+// if (weather === "Rain") {
+//   console.log("Take Umbrella");
+
+// } else {
+//   console.log("Enjoy the sunshine!!!!");
+
+// }
