d8977e211ab22b4efc9eda2d0f44ca6ec53499f9
jkhiba2023 <khan.jabir1055@gmail.com>
2025-09-18 01:59:40 +0530

    Check age and pass

--- Day4/script.js
+++ Day4/script.js
@@ -9,14 +9,31 @@
 
 // }
 
+
+
 /*If its raining, take umbrella. Otherwise, enjoy the sunshine!!!*/
 
 // let weather = "Sunny";
 
 // if (weather === "Rain") {
 //   console.log("Take Umbrella");
-
 // } else {
 //   console.log("Enjoy the sunshine!!!!");
+// }
 
+/*Write a program to check a person age is 18 or not. If age is 18 then check that they have pass or
+not*/
+
+// let age = 20;
+// let hasPass = false;
+
+// if (age >= 18) {
+//   if (hasPass) {
+//     console.log("Welcome to the club");
+//   } else {
+//     console.log("Sorry!!! Without Pass you are not allowed");
+//   }
+
+// } else {
+//   console.log("You are restricted allowed");
 // }
