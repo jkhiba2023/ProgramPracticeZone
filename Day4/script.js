8b62c0c7f70195257ad4b81bfb182e422eb39d02
jkhiba2023 <khan.jabir1055@gmail.com>
2025-09-18 01:28:26 +0530

    Stock checker

---
+++ Day4/script.js
@@ -0,0 +1,10 @@
+/*Write a program to check if the stock of a product is 0. If it is, display the message
+"Product is out of stock."*/
+
+let ProductStock = 0;
+if (ProductStock === 0) {
+  console.log("Product is out of stock");
+} else {
+  console.log("Stock count is:", ProductStock);
+
+}
