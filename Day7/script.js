906c10aad42169334b0187c2efb688fc71e0d12c
jkhiba2023 <khan.jabir1055@gmail.com>
2025-10-16 01:26:02 +0530

    Check fizz Buzz or FizzBuzz

---
+++ Day7/script.js
@@ -0,0 +1,13 @@
+/*Check if Number is Divisible by 3 or 5 or Both. Print "Fizz" for multiples of 3, "Buzz" for multiples of 5,
+"FizzBuzz" for both.*/
+
+// let userNumber = Number(prompt("Enter Number "));
+// if (userNumber % 3 === 0 && userNumber % 5 === 0) {
+//   console.log("FizzBuzz");
+// } else if (userNumber % 3 === 0) {
+//   console.log("Fizz");
+// } else if (userNumber % 5 === 0) {
+//   console.log("Buzz");
+// } else {
+//   console.log("Invalid Input");
+// }
