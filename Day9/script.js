a97b8bf92b96d6bb245eeaf19eeda55dac6af7b4
jkhiba2023 <khan.jabir1055@gmail.com>
2025-10-28 01:27:05 +0530

    Checking odd numbers

--- Day9/script.js
+++ Day9/script.js
@@ -6,4 +6,22 @@
 // }
 // console.log(sum);
 
+/*Calculate the sum of numbers from 'm' to 'n" */
+// let sum = 0;
+// let n = 15;
+// for (let m = 5; m <= n; m++) {
+//   sum += m;
+//   console.log(sum);
+// }
+// console.log(sum);
+
+/*Print all odd numbers from 0 to 'n' */
+
+// for (let i = 0; i <= 20; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+
 
