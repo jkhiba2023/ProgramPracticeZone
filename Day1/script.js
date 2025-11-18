9f80d5401ab82678bda753104ab6fac3777f1aea
jkhiba2023 <khan.jabir1055@gmail.com>
2025-11-18 16:34:37 +0530

    vowels and consonents are in a given string using for of loop

--- Day1/script.js
+++ Day1/script.js
@@ -1,8 +1,26 @@
 /*Print all even numbers from 0 to 100*/
 
-for (let i = 0; i < 100; i++) {
-  if (i % 2 === 0) {
-    console.log("even number is", i);
+// for (let i = 0; i < 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log("even number is", i);
 
+//   }
+// }
+
+/*Calculate how many vowels and consonents are in a given string using for of loop.*/
+
+let str = "Jabir Khan HK";
+let vowel = 0;
+let consonents = 0;
+
+for (let ch of str) {
+  if (ch === 'a' || ch === 'e' || ch === 'i' || ch === 'o' || ch === 'u' || ch === 'A' || ch === 'E' || ch === 'I' || ch === "O" || ch === 'U') {
+    vowel++;
+  }
+  else if (ch === " ") { }
+  else {
+    consonents++;
   }
 }
+console.log('Vowel count:', vowel);
+console.log('Consonents count:', consonents);
