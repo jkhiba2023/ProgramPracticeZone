1fd9624cc211eb24fb20fa35d664ec836b01782b
jkhiba2023 <khan.jabir1055@gmail.com>
2025-11-18 16:40:57 +0530

    sum of n numbers

--- Day1/script.js
+++ Day1/script.js
@@ -9,18 +9,26 @@
 
 /*Calculate how many vowels and consonents are in a given string using for of loop.*/
 
-let str = "Jabir Khan HK";
-let vowel = 0;
-let consonents = 0;
+// let str = "Jabir Khan HK";
+// let vowel = 0;
+// let consonents = 0;
 
-for (let ch of str) {
-  if (ch === 'a' || ch === 'e' || ch === 'i' || ch === 'o' || ch === 'u' || ch === 'A' || ch === 'E' || ch === 'I' || ch === "O" || ch === 'U') {
-    vowel++;
-  }
-  else if (ch === " ") { }
-  else {
-    consonents++;
-  }
-}
-console.log('Vowel count:', vowel);
-console.log('Consonents count:', consonents);
+// for (let ch of str) {
+//   if (ch === 'a' || ch === 'e' || ch === 'i' || ch === 'o' || ch === 'u' || ch === 'A' || ch === 'E' || ch === 'I' || ch === "O" || ch === 'U') {
+//     vowel++;
+//   }
+//   else if (ch === " ") { }
+//   else {
+//     consonents++;
+//   }
+// }
+// console.log('Vowel count:', vowel);
+// console.log('Consonents count:', consonents);
+
+/*Calculate sum of n numbers*/
+
+// let sum = 0;
+// for (let i = 1; i <= 5; i++) {
+//   sum += i;
+// }
+// console.log(sum);
