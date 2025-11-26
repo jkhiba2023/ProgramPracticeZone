4c45f8f953ae2c589021b5c0bcb67c0ee552a120
jkhiba2023 <khan.jabir1055@gmail.com>
2025-11-26 17:52:56 +0530

    reverse the string

--- Day2/script.js
+++ Day2/script.js
@@ -2,13 +2,33 @@
 //Input: "i am writing js code."
 //Output:"I Am Writing Js Code."
 
-let str1 = "i am writing js code";
+// let str1 = "i am writing js code";
 
-function letterCap(str1) {
-  let arrWords = str1.split(" ");
-  for (let i = 0; i < arrWords.length; i++) {
-    arrWords[i] = arrWords[i].charAt(0).toUpperCase() + arrWords[i].slice(1, arrWords.length);
-  }
-  return arrWords.join(" ");
-}
-console.log(letterCap(str1));
+// function letterCap(str1) {
+//   let arrWords = str1.split(" ");
+//   for (let i = 0; i < arrWords.length; i++) {
+//     arrWords[i] = arrWords[i].charAt(0).toUpperCase() + arrWords[i].slice(1, arrWords.length);
+//   }
+//   return arrWords.join(" ");
+// }
+
+// function letterCap(str1) {
+//   return str1.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+// }
+
+// console.log(letterCap(str1));
+
+//reverse the strings
+
+// let str = "I am writing Js Reverse Js Code";
+
+// function reverseStr(str) {
+//   let arr = str.split(" ");
+//   let res = " ";
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     res += arr[i] + " ";
+//   }
+//   return res;
+// }
+
+// console.log(reverseStr(str));
