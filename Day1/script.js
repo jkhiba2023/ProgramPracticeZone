f8e2434dd1bba85ad1021b6c0bbbef20ab8fcb19
jkhiba2023 <khan.jabir1055@gmail.com>
2025-12-17 16:39:43 +0530

    Find reverse string

--- Day1/script.js
+++ Day1/script.js
@@ -1,73 +1,20 @@
-/*Print all even numbers from 0 to 100*/
+/*Reverse a string*/
 
-// for (let i = 0; i < 100; i++) {
-//   if (i % 2 === 0) {
-//     console.log("even number is", i);
+// let str="Jabir";
+// let newStr=str.split("")
+// console.log(newStr);
 
-//   }
+// let res=str.split("").reverse().join('');
+// console.log(res);
+
+
+
+// function reverseStr(str){
+//  let res="";
+//   for(let i=newStr.length-1;i>=0;i--){
+//     res+=newStr[i];
+// }
+//   return res;
 // }
 
-/*Calculate how many vowels and consonents are in a given string using for of loop.*/
-
-// let str = "Jabir Khan HK";
-// let vowel = 0;
-// let consonents = 0;
-
-// for (let ch of str) {
-//   if (ch === 'a' || ch === 'e' || ch === 'i' || ch === 'o' || ch === 'u' || ch === 'A' || ch === 'E' || ch === 'I' || ch === "O" || ch === 'U') {
-//     vowel++;
-//   }
-//   else if (ch === " ") { }
-//   else {
-//     consonents++;
-//   }
-// }
-// console.log('Vowel count:', vowel);
-// console.log('Consonents count:', consonents);
-
-/*Calculate sum of n numbers*/
-
-// let sum = 0;
-// for (let i = 1; i <= 5; i++) {
-//   sum += i;
-// }
-// console.log(sum);
-
-/*find the largest number*/
-
-// let arr = [27, 88, 72, 34, 78, 23];
-
-
-// function largestNum(arr) {
-//   let largestNumber = 0;
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] > largestNumber) {
-//       largestNumber = arr[i];
-//     }
-//   }
-//   return largestNumber
-// }
-
-// console.log(largestNum(arr));
-
-
-/*find the second largest number*/
-
-let arr = [323, 101, 47, 34, 177, 992];
-
-
-function secondLarge(arr) {
-  let largestNum = 0;
-  let secondLargestNum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > largestNum) {
-      secondLargestNum = largestNum;
-      largestNum = arr[i];
-    } else if (arr[i] > secondLargestNum && arr[i] !== largestNum) {
-      secondLargestNum = arr[i]
-    }
-  }
-  return secondLargestNum;
-}
-
-console.log(secondLarge(arr));
+// console.log(reverseStr(newStr));
