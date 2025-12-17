2f6c267518d74b4cf6c1123e8ce4cd4c8c10ce8b
jkhiba2023 <khan.jabir1055@gmail.com>
2025-12-17 19:04:22 +0530

    check the string is vowel or not and print its value

--- Day1/script.js
+++ Day1/script.js
@@ -21,18 +21,36 @@
 
 //Find the largest Number in an array
 
-let arr = [12, 44, 28, 11, 16];
+// let arr = [12, 44, 28, 11, 16];
 
 
-function largestNum(arr) {
-  let max = arr[0];
+// function largestNum(arr) {
+//   let max = arr[0];
 
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-  }
-  return max;
-}
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+//   return max;
+// }
 
-console.log(largestNum(arr));
+// console.log(largestNum(arr));
+
+//check the give string is a vowel. if yes than return the vowel value.
+
+// let str="gulam";
+
+// function checkVowelVal(str){
+//   let vowels="aeiouAEIOU";
+//   let res='';
+  
+//   for(let i=0;i<str.length;i++){
+//     if(vowels.includes(str[i])){
+//       res+=str[i];
+//     }
+//   }
+//   return res
+// }
+
+// console.log(checkVowelVal(str));
