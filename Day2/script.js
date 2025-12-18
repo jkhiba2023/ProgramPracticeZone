908116ab526ded5f35286a55b5a571585cb6a624
jkhiba2023 <khan.jabir1055@gmail.com>
2025-12-18 17:40:52 +0530

    find second largest of an array

---
+++ Day2/script.js
@@ -0,0 +1,20 @@
+/*Find the second largest Value */
+
+// let arr = [45, 12, 88, 3, 67, 21, 99, 54, 7, 32];
+
+// function secondLargest(arr) {
+//   let largest = 0;
+//   let secondLarge = 0;
+
+//   for (let i = 0; i <= arr.length; i++) {
+//     if (arr[i] > largest) {
+//       secondLarge = largest;
+//       largest = arr[i];
+//     } else if (arr[i] > secondLarge) {
+//       secondLarge = arr[i];
+//     }
+//   }
+//   return secondLarge;
+// }
+
+// console.log(secondLargest(arr));
