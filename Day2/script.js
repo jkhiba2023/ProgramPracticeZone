0dda24e87561a2382a9426490eaf3183813245aa
jkhiba2023 <khan.jabir1055@gmail.com>
2025-12-18 19:01:01 +0530

    remove the duplicate element of an array

--- Day2/script.js
+++ Day2/script.js
@@ -18,3 +18,21 @@
 // }
 
 // console.log(secondLargest(arr));
+
+/*Remove duplicate elements from an array.*/
+
+// let arr1 = [1, 2, 3, 2, 4, 5, 4, 5];
+
+// function duplicate(arr) {
+//   let dual = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!dual.includes(arr[i])) {
+//       dual.push(arr[i]);
+//     }
+//   }
+//   return dual;
+// }
+
+// console.log(duplicate(arr1))
+
