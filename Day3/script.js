64d0c723316eb84ed43422be2f1fde4f1855b4e4
jkhiba2023 <khan.jabir1055@gmail.com>
2025-12-25 03:07:34 +0530

    seting the time out function for next every 5 second

--- Day3/script.js
+++ Day3/script.js
@@ -2,10 +2,17 @@
 capitalized*/
 
 
-let str = "kabir"
+// let str = "kabir"
 
-function firstLetter(str) {
-  return str[0].toUpperCase() + str.slice(1);
-}
+// function firstLetter(str) {
+//   return str[0].toUpperCase() + str.slice(1);
+// }
 
-console.log(firstLetter(str));
+// console.log(firstLetter(str));
+
+
+// Show an alert message that says "Please Login" after 5 sec on your website.
+
+// setInterval(function () {
+//   alert("Please Log In")
+// }, 5000);
