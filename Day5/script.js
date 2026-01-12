36ebf50b06080dd1af1a3314c415800ab2a5a9c6
jkhiba2023 <khan.jabir1055@gmail.com>
2026-01-12 16:59:19 +0530

    return reduce method

---
+++ Day5/script.js
@@ -0,0 +1,15 @@
+/*Example of reduce method in an array*/
+
+// const arr = [1, 2, 3, 4, 5];
+
+// function reduceF(arr) {
+//   const initialValue = 0;
+
+//   const finalVal = arr.reduce((totalVal, currentVal) => {
+//     return totalVal + currentVal
+//   }, initialValue);
+
+//   return finalVal
+// }
+
+// console.log(reduceF(arr));
